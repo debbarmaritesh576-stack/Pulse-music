91f5c50362f6d307033c88a5f1500d9445559d6c
Tab browser <debbarmaritesh576@gmail.com>
2026-05-20 01:48:40 +0530

    Create feature-lyrics/build.gradle.kts

---
+++ feature-lyrics/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pulse.music.lyrics"
+    compileSdk = 34
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.5" }
+}
+
+dependencies {
+    implementation(project(":common-ui"))
+    implementation(project(":common-util"))
+    implementation(project(":core-player"))
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.icons)
+    implementation(libs.hilt.android)
+}
