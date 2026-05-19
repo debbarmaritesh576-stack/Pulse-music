2913b72d00ff7160116c883418c03ed6cde24061
Tab browser <debbarmaritesh576@gmail.com>
2026-05-20 01:28:25 +0530

    Create feature-search/build.gradle.kts

---
+++ feature-search/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pulse.music.search"
+    compileSdk = 34
+    defaultConfig { minSdk = 26 }
+    buildFeatures { compose = true }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.5" }
+}
+
+dependencies {
+    implementation(project(":common-ui"))
+    implementation(project(":common-util"))
+    implementation(project(":core-database"))
+    implementation(project(":core-player"))
+    implementation(project(":core-mediastore"))
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.icons)
+    implementation(libs.hilt.android)
+}
