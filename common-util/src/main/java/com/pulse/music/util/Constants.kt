ef9f8937d355d874d2e514ba2423c57d360f0d5b
Tab browser <debbarmaritesh576@gmail.com>
2026-05-19 21:00:11 +0530

    Create common-util/src/main/java/com/pulse/music/util/Constants.kt

---
+++ common-util/src/main/java/com/pulse/music/util/Constants.kt
@@ -0,0 +1,19 @@
+package com.pulse.music.util  
+  
+object Constants {  
+    const val APP_NAME = "Pulse Music"  
+    const val PACKAGE_NAME = "com.pulse.music"  
+    const val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=$PACKAGE_NAME"  
+    const val PRIVACY_POLICY_URL = "https://pulsemusic.app/privacy"  
+      
+    const val NOTIFICATION_CHANNEL_ID = "pulse_music_playback"  
+    const val NOTIFICATION_ID = 1001  
+    const val WIDGET_UPDATE_ACTION = "com.pulse.music.WIDGET_UPDATE"  
+      
+    const val DEFAULT_SHUFFLE = false  
+    const val DEFAULT_REPEAT_MODE = 1 // REPEAT_ALL  
+      
+    const val SLEEP_TIMER_15 = 15L  
+    const val SLEEP_TIMER_30 = 30L  
+    const val SLEEP_TIMER_60 = 60L  
+}
