21c53c7991f9f509c2115680049e5ff412a4e406
Tab browser <debbarmaritesh576@gmail.com>
2026-05-19 23:07:37 +0530

    Create core-network/src/main/java/com/pulse/music/network/RetrofitClient.kt

---
+++ core-network/src/main/java/com/pulse/music/network/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.pulse.music.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class RetrofitClient @Inject constructor() {
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .readTimeout(15, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    fun <T> createService(serviceClass: Class<T>, baseUrl: String): T {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(serviceClass)
+    }
+}
