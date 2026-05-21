123116b7ca11bf399b8943e6141b9adbfd7e5f93
Tab browser <debbarmaritesh576@gmail.com>
2026-05-21 23:14:51 +0530

    Update Colors.kt

--- common-ui/src/main/java/com/pulse/music/ui/theme/Colors.kt
+++ common-ui/src/main/java/com/pulse/music/ui/theme/Colors.kt
@@ -2,19 +2,36 @@ package com.pulse.music.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Dark Theme
+val BackgroundDark = Color(0xFF0B0B0F)
+val SurfaceDark = Color(0xFF15161B)
+val SurfaceVariantDark = Color(0xFF1E1F25)
 
-val Purple40 = Color(0xFF7C3AED)
-val PurpleGrey40 = Color(0xFF625B71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlue = Color(0xFF5B8CFF)
+val PrimaryPurple = Color(0xFF8B5CF6)
+val AccentPink = Color(0xFFFF4D8D)
 
-val BackgroundDark = Color(0xFF09090B)
-val SurfaceDark = Color(0xFF111114)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFB5B8C5)
+val TextMuted = Color(0xFF7B8092)
 
-val AccentBlue = Color(0xFF06B6D4)
-val AccentPurple = Color(0xFF8B5CF6)
+val SuccessGreen = Color(0xFF22C55E)
+val ErrorRed = Color(0xFFEF4444)
 
-val TextPrimary = Color.White
-val TextSecondary = Color(0xFFB3B3B3)
+// Light Theme
+val BackgroundLight = Color(0xFFF6F7FB)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceVariantLight = Color(0xFFE9ECF5)
+
+val TextPrimaryLight = Color(0xFF111318)
+val TextSecondaryLight = Color(0xFF4B5563)
+
+val DarkGradientStart = Color(0xFF17181F)
+val DarkGradientEnd = Color(0xFF0F1015)
+
+val PlayerGradientTop = Color(0xFF1D2671)
+val PlayerGradientBottom = Color(0xFFC33764)
+
+val MiniPlayerBackground = Color(0xFF191A20)
+
+val CardBorder = Color(0x22FFFFFF)
